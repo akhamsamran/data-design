c449e6ccd0ac31bfd231733abfc5c08103db7bc1
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-13 13:08:25 -0700

    trying to fix error with character set for name

--- sql/medium.sql
+++ sql/medium.sql
@@ -1,5 +1,5 @@
 -- set database collation to UTF-8
-ALTER DATABASE akhamsamran1_CHANGE_ME CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER DATABASE akhamsamran1 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 -- this is the table for profile:
 CREATE TABLE profile(
