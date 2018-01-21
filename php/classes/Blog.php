5affd2e3353c6649a1d398bc956ec874aa895984
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-21 10:06:59 -0700

    added access and mutator methods for blogid and blogProfileId in blog.php

--- php/classes/Blog.php
+++ php/classes/Blog.php
@@ -11,6 +11,7 @@
  * this blog contains all the articles or blogs written by the members listed in the profile.  Blog includes title, blog content, Date, and the author.  Other information could be added, such as topics, subtitles, etc.
  *
  * @author Anna Khamsamran <akhamsamran1@cnm.edu>
+ * @see Dylan McDonald <dmcdonald21@cnm.edu>
  *
  **/
 class blog{
@@ -36,8 +37,57 @@ class blog{
 	 **/
 	private $blogDate;
 	/**
+	 *accessor method for blog id
 	 *
+	 * @returns string value for blog id
+	 **/
+	public function getBlogId() {
+		return $this->blogId;
+	}
+	/**
+	 * mutator method for blog id
+	 *
+	 * @param Uuid /string $newBlogId for the new value of the blog id
+	 * @throws InvalidArgumentException if $newBlogId is not positive
+	 * @throws TypeError if $newBlogId is not an integer
+	 **/
+	public function setBlogId($newBlogId): void {
+		try {
+			$uuid = self::validateUuid($newBlogId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+		// store and convert the blog id
+		$this->blogId = $uuid;
+	}
+	/**
+	 * accessor method for blog profile id
+	 *
+	 * @returns int $newBlogProfileId
 	 */
+	public function getBlogProfileId() {
+		return $this->blogProfileId;
+	}
+	/**
+	 * mutator method for blog profile id
+	 *
+	 * @param  Uuid/string $newBlogProfileId new value of blog profile id
+	 * * @throws RangeException if $newProfileId is not positive
+	 * @throws TypeError if $newTweetProfileId is not an integer
+	 **/
+	public function setBlogProfileId( $newBlogProfileId) : void {
+		try {
+			$uuid = self::validateUuid($newBlogProfileId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+
+		// convert and store the profile id
+		$this->tweetProfileId = $uuid;
+	}
+
 
 
 
