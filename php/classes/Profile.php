bc4f2a96ac1fd3eaf914ee8ddbbaf98dd8416b57
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-20 20:39:15 -0700

    access and mutator for profile activation token

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -27,7 +27,7 @@ class Profile {
 	/**
 	 * access token for the profile
 	 **/
-	private $profileAccessToken;
+	private $profileActivationToken;
 	/**
 	 * email for the profile
 	 **/
@@ -50,11 +50,87 @@ class Profile {
 	private $profileSalt;
 
 	/**
+	 * accessor method for profile id
+	 *
+	 * @return int value of profile id
+	 **/
+	public function getProfileId(){
+		return($this->profileId);
+	}
+
+	/**
+	 * mutator method for profile id
+	 *
+	 * @param int $newProfileId new value of profile id
+	 * @throws UnexpectedValueException if $newProfileId is not an integer
+	 **/
+	public function setProfileId($newProfileId){
+		$newProfileId = filter_var($newProfileId, FILTER_VALIDATE_INT);
+		if($newProfileId === false) {
+			throw(new UnexpectedValueException("profile id is not a valid integer"));
+		}
+		//convert and store the profile id
+		$this->profileId = intval($newProfileId);
+	}
+	/**
+	 * accessor method for profile about me
+	 *
+	 * @return string value of profile about me
+	 **/
+	public function getprofileAboutMe(){
+		return ($this->profileAboutMe);
+	}
+	/**
+	 * mutator method for profile about me
+	 *
+	 * @param string $newProfileAboutMe new value of profile about me
+	 * @throws UnexpectedValueException if $newProfileAboutMe is not a string
+	 **/
+	public function setProfileAboutMe($newProfileAboutMe) {
+		$newProfileAboutMe = filter_var($newProfileAboutMe, FILTER_SANITIZE_STRING);
+		if($newProfileAboutMe === false) {
+			throw(new UnexpectedValueException("About me is not a valid string"));
+		}
+		//convert and store the profile about me
+		$this->profileAboutMe = $newProfileAboutMe;
+	}
+
+	/**accessor method for profile activation token
+	 *
+	 * @return string value of profile activation token
+	 **/
+	public function getProfileActivationToken() {
+		return ($this->profileActivationToken);
+	}
+	/**
+	 * mutator method for profile access token
+	 *
+	 * @param string ?$newProfileActivationToken new value of access token
+	 * @throws UnexpectedValueException if $newProfileActivationToken is not a string
+	 **/
+	public function setProfileActivationToken($newProfileActivationToken) {
+		$newProfileActivationToken = filter_var($newProfileActivationToken, FILTER_SANITIZE_STRING);
+		if($newProfileActivationToken === false) {
+			throw(new UnexpectedValueException("Activation token is not a valid string"));
+		}
+		//convert and store profile activation token
+		$this->profileActivationToken = $newProfileActivationToken
+	}
+
+
+
+
+
+
+
+
+
+	/**
 	 * constructor for this profile
 	 *
 	 * @param int $newProfileId new profile id
 	 * @param string $newProfileAboutMe
-	 * @param string $newProfileAccessToken
+	 * @param string $newProfileActivationToken
 	 * @param string $newProfileEmail
 	 * @param string $newProfileFirstName
 	 * @param string $newProfileHash
@@ -62,16 +138,16 @@ class Profile {
 	 * @param String $newProfileSalt
 	 * @throws UnexpectedValueException if any of the parameters are invalid
 	 **/
-	public function __construct($newProfileId, $newProfileAboutMe, $newProfileAccessToken, $newProfileEmail,$newProfileFirstName, $newProfileHash, $newProfileLastName, $newProfileSalt) {
+	public function __construct($newProfileId, $newProfileAboutMe, $newProfileActivationToken, $newProfileEmail,$newProfileFirstName, $newProfileHash, $newProfileLastName, $newProfileSalt) {
 		try{
 			$this->setProfileId($newProfileId);
 			$this->setProfileAboutMe($newProfileAboutMe);
-			$this->profileAccessToken($newProfileAccessToken);
-			$this->profileEmail($newProfileEmail);
-			$this->profileFirstName($newProfileFirstName);
-			$this->profileHash($newProfileHash);
-			$this->profileLastName($newProfileLastName);
-			$this->profileSalt($newProfileSalt);
+			$this->setprofileActivationToken($newProfileActivationToken);
+			$this->setprofileEmail($newProfileEmail);
+			$this->setprofileFirstName($newProfileFirstName);
+			$this->setprofileHash($newProfileHash);
+			$this->setprofileLastName($newProfileLastName);
+			$this->setprofileSalt($newProfileSalt);
 		} catch(UnexpectedValueException $exception){
 			//rethrow to the caller
 		throw(new UnexpectedValueException("Unable to construct Profile", 0, $exception));
